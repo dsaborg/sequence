c64c18af45412d7368e962f2e29b3bf052311d41
Daniel Skogquist Åborg <daniel.aborg@purplescout.se>
2017-01-14 15:57:39 +0100

    Increase test coverage.

--- src/test/java/org/d2ab/util/StrictTest.java
+++ src/test/java/org/d2ab/util/StrictTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
 
 public class StrictTest {
 	@Test
@@ -30,5 +31,26 @@ public class StrictTest {
 
 		Strict.reset();
 		assertThat(Strict.LENIENT, is(lenient));
+	}
+
+	@Test
+	public void check() {
+		boolean lenient = !Boolean.getBoolean(Strict.STRICT_PROPERTY);
+
+		assumeThat(lenient, is(false));
+
+		boolean failed;
+		try {
+			Strict.check();
+			failed = false;
+		} catch (AssertionError expected) {
+			failed = true;
+		}
+
+		assertThat(failed, is(true));
+
+		Strict.unset();
+		Strict.check();
+		Strict.reset();
 	}
 }
