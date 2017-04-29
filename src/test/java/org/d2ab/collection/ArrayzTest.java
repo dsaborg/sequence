eb515ed571e5bb7476ad5450dce843b8fe38a151
Daniel Skogquist Åborg <daniel.aborg@purplescout.se>
2017-04-29 19:39:16 +0200

    Increase test coverage.

--- src/test/java/org/d2ab/collection/ArrayzTest.java
+++ src/test/java/org/d2ab/collection/ArrayzTest.java
@@ -2,6 +2,10 @@ package org.d2ab.collection;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.hamcrest.Matchers.*;
@@ -17,49 +21,49 @@ public class ArrayzTest {
 	}
 
 	@Test
-	public void forEach() throws Exception {
+	public void forEach() {
 		AtomicInteger i = new AtomicInteger();
 		Arrayz.forEach(new Integer[]{1, 2, 3, 4, 5}, x -> assertThat(x, is(i.getAndIncrement() + 1)));
 		assertThat(i.get(), is(5));
 	}
 
 	@Test
-	public void reverse() throws Exception {
+	public void reverse() {
 		Integer[] xs = {1, 2, 3, 4, 5};
 		assertThat(Arrayz.reverse((Object[]) xs), is(sameInstance(xs)));
 		assertThat(xs, is(arrayContaining(5, 4, 3, 2, 1)));
 	}
 
 	@Test
-	public void reverseLongs() throws Exception {
+	public void reverseLongs() {
 		long[] xs = {1, 2, 3, 4, 5};
 		assertThat(Arrayz.reverse(xs), is(sameInstance(xs)));
 		assertArrayEquals(new long[]{5, 4, 3, 2, 1}, xs);
 	}
 
 	@Test
-	public void reverseInts() throws Exception {
+	public void reverseInts() {
 		int[] xs = {1, 2, 3, 4, 5};
 		assertThat(Arrayz.reverse(xs), is(sameInstance(xs)));
 		assertArrayEquals(new int[]{5, 4, 3, 2, 1}, xs);
 	}
 
 	@Test
-	public void reverseDoubles() throws Exception {
+	public void reverseDoubles() {
 		double[] xs = {1, 2, 3, 4, 5};
 		assertThat(Arrayz.reverse(xs), is(sameInstance(xs)));
 		assertArrayEquals(new double[]{5, 4, 3, 2, 1}, xs, 0);
 	}
 
 	@Test
-	public void reverseChars() throws Exception {
+	public void reverseChars() {
 		char[] xs = {'a', 'b', 'c', 'd', 'e'};
 		assertThat(Arrayz.reverse(xs), is(sameInstance(xs)));
 		assertArrayEquals(new char[]{'e', 'd', 'c', 'b', 'a'}, xs);
 	}
 
 	@Test
-	public void contains() throws Exception {
+	public void contains() {
 		Integer[] xs = {1, 2, 3, 4, 5};
 		for (Integer x : xs)
 			assertThat(Arrayz.contains(xs, x), is(true));
@@ -68,7 +72,7 @@ public class ArrayzTest {
 	}
 
 	@Test
-	public void containsLong() throws Exception {
+	public void containsLong() {
 		long[] xs = {1, 2, 3, 4, 5};
 		for (long x : xs)
 			assertThat(Arrayz.contains(xs, x), is(true));
@@ -77,7 +81,7 @@ public class ArrayzTest {
 	}
 
 	@Test
-	public void containsInt() throws Exception {
+	public void containsInt() {
 		int[] xs = {1, 2, 3, 4, 5};
 		for (int x : xs)
 			assertThat(Arrayz.contains(xs, x), is(true));
@@ -86,7 +90,7 @@ public class ArrayzTest {
 	}
 
 	@Test
-	public void containsDoubleExactly() throws Exception {
+	public void containsDoubleExactly() {
 		double[] xs = {1, 2, 3, 4, 5};
 		for (double x : xs)
 			assertThat(Arrayz.containsExactly(xs, x), is(true));
@@ -95,7 +99,7 @@ public class ArrayzTest {
 	}
 
 	@Test
-	public void containsDouble() throws Exception {
+	public void containsDouble() {
 		double[] xs = {1, 2, 3, 4, 5};
 		for (double x : xs) {
 			assertThat(Arrayz.contains(xs, x, 0), is(true));
@@ -107,11 +111,25 @@ public class ArrayzTest {
 	}
 
 	@Test
-	public void containsChar() throws Exception {
+	public void containsChar() {
 		char[] xs = {'a', 'b', 'c', 'd', 'e'};
 		for (char x : xs)
 			assertThat(Arrayz.contains(xs, x), is(true));
 		assertThat(Arrayz.contains(xs, ' '), is(false));
 		assertThat(Arrayz.contains(xs, 'q'), is(false));
+	}
+
+	@Test
+	public void shuffle() {
+		Set<Integer> permutations = new HashSet<>();
+		Random random = new Random(17);
+
+		for (int i = 0; i < 100; i++) {
+			Integer[] array = {1, 2, 3, 4};
+			Arrayz.shuffle(array, random);
+			permutations.add(Arrays.hashCode(array));
+		}
+
+		assertThat(permutations.size(), is(24));
 	}
 }
