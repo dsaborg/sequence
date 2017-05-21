1a55b0acce9b945d9f766a5cc4533cb261bf3621
Daniel Skogquist Åborg <daniel.aborg@purplescout.se>
2017-05-21 18:24:59 +0200

    Improve test coverage.

--- src/test/java/org/d2ab/collection/IterableCollectionTest.java
+++ src/test/java/org/d2ab/collection/IterableCollectionTest.java
@@ -24,6 +24,7 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableCollection;
 import static org.d2ab.collection.SizedIterable.SizeType.UNAVAILABLE;
 import static org.d2ab.test.HasSizeCharacteristics.*;
 import static org.d2ab.test.Tests.expecting;
@@ -34,11 +35,13 @@ import static org.junit.Assert.assertThat;
 public class IterableCollectionTest {
 	private final IterableCollection<Integer> empty = IterableCollection.empty();
 	private final IterableCollection<Integer> single = IterableCollection.of(1);
-	private final IterableCollection<Integer> regular = IterableCollection.of(1, 2, 3, 4, 5);
+	private final IterableCollection<Integer> fixed = IterableCollection.of(1, 2, 3, 4, 5);
 	private final IterableCollection<Integer> mutable = IterableCollection.from(
 			new ArrayList<>(Lists.of(1, 2, 3, 4, 5)));
 	private final IterableCollection<Integer> sized = IterableCollection.from(
-			Iterables.of(1, 2, 3, 4, 5));
+			unmodifiableCollection(new ArrayList<>(Lists.of(1, 2, 3, 4, 5))));
+	private final IterableCollection<Integer> unsized = IterableCollection.from(
+			Iterables.of(1, 2, 3, 4, 5)::iterator);
 
 	@Test
 	public void fromSizedIterableAsIterable() {
@@ -117,37 +120,40 @@ public class IterableCollectionTest {
 
 	@Test
 	public void iteration() {
-		assertThat(empty, is(emptyIterable()));
+		assertThat(empty, is(emptyFixedIterable()));
 		expecting(NoSuchElementException.class, () -> empty.iterator().next());
 
-		assertThat(single, contains(1));
+		assertThat(single, containsFixed(1));
 		Iterator<Integer> singleIterator = single.iterator();
 		assertThat(singleIterator.hasNext(), is(true));
 		assertThat(singleIterator.next(), is(1));
 		assertThat(singleIterator.hasNext(), is(false));
 		expecting(NoSuchElementException.class, singleIterator::next);
 
-		assertThat(regular, contains(1, 2, 3, 4, 5));
-		assertThat(mutable, contains(1, 2, 3, 4, 5));
-		assertThat(sized, contains(1, 2, 3, 4, 5));
+		assertThat(fixed, containsFixed(1, 2, 3, 4, 5));
+		assertThat(mutable, containsSized(1, 2, 3, 4, 5));
+		assertThat(sized, containsSized(1, 2, 3, 4, 5));
+		assertThat(unsized, containsUnsized(1, 2, 3, 4, 5));
 	}
 
 	@Test
 	public void size() {
 		assertThat(empty.size(), is(0));
 		assertThat(single.size(), is(1));
-		assertThat(regular.size(), is(5));
+		assertThat(fixed.size(), is(5));
 		assertThat(mutable.size(), is(5));
 		assertThat(sized.size(), is(5));
+		assertThat(unsized.size(), is(5));
 	}
 
 	@Test
 	public void isEmpty() {
 		assertThat(empty.isEmpty(), is(true));
 		assertThat(single.isEmpty(), is(false));
-		assertThat(regular.isEmpty(), is(false));
+		assertThat(fixed.isEmpty(), is(false));
 		assertThat(mutable.isEmpty(), is(false));
 		assertThat(sized.isEmpty(), is(false));
+		assertThat(unsized.isEmpty(), is(false));
 	}
 
 	@Test
@@ -157,9 +163,9 @@ public class IterableCollectionTest {
 		assertThat(single.contains(17), is(false));
 		assertThat(single.contains(1), is(true));
 
-		assertThat(regular.contains(17), is(false));
+		assertThat(fixed.contains(17), is(false));
 		for (int i = 1; i <= 5; i++)
-			assertThat(regular.contains(i), is(true));
+			assertThat(fixed.contains(i), is(true));
 
 		assertThat(mutable.contains(17), is(false));
 		for (int i = 1; i <= 5; i++)
@@ -168,33 +174,60 @@ public class IterableCollectionTest {
 		assertThat(sized.contains(17), is(false));
 		for (int i = 1; i <= 5; i++)
 			assertThat(sized.contains(i), is(true));
+
+		assertThat(unsized.contains(17), is(false));
+		for (int i = 1; i <= 5; i++)
+			assertThat(unsized.contains(i), is(true));
 	}
 
 	@Test
 	public void toArray() {
 		assertThat(empty.toArray(), is(emptyArray()));
 		assertThat(single.toArray(), is(arrayContaining(1)));
-		assertThat(regular.toArray(), is(arrayContaining(1, 2, 3, 4, 5)));
+		assertThat(fixed.toArray(), is(arrayContaining(1, 2, 3, 4, 5)));
 		assertThat(mutable.toArray(), is(arrayContaining(1, 2, 3, 4, 5)));
 		assertThat(sized.toArray(), is(arrayContaining(1, 2, 3, 4, 5)));
+		assertThat(unsized.toArray(), is(arrayContaining(1, 2, 3, 4, 5)));
 	}
 
 	@Test
-	public void toArrayOfType() {
+	public void toArrayWithEmptyArray() {
 		assertThat(empty.toArray(new Integer[0]), is(emptyArray()));
 		assertThat(single.toArray(new Integer[0]), is(arrayContaining(1)));
-		assertThat(regular.toArray(new Integer[0]), is(arrayContaining(1, 2, 3, 4, 5)));
+		assertThat(fixed.toArray(new Integer[0]), is(arrayContaining(1, 2, 3, 4, 5)));
 		assertThat(mutable.toArray(new Integer[0]), is(arrayContaining(1, 2, 3, 4, 5)));
 		assertThat(sized.toArray(new Integer[0]), is(arrayContaining(1, 2, 3, 4, 5)));
+		assertThat(unsized.toArray(new Integer[0]), is(arrayContaining(1, 2, 3, 4, 5)));
+	}
+
+	@Test
+	public void toArrayWithSmallerArray() {
+		assertThat(empty.toArray(new Integer[0]), is(emptyArray()));
+		assertThat(single.toArray(new Integer[0]), is(arrayContaining(1)));
+		assertThat(fixed.toArray(new Integer[3]), is(arrayContaining(1, 2, 3, 4, 5)));
+		assertThat(mutable.toArray(new Integer[3]), is(arrayContaining(1, 2, 3, 4, 5)));
+		assertThat(sized.toArray(new Integer[3]), is(arrayContaining(1, 2, 3, 4, 5)));
+		assertThat(unsized.toArray(new Integer[3]), is(arrayContaining(1, 2, 3, 4, 5)));
+	}
+
+	@Test
+	public void toArrayWithLargerArray() {
+		assertThat(empty.toArray(Arrayz.fill(new Integer[2], 17)), is(arrayContaining(null, 17)));
+		assertThat(single.toArray(Arrayz.fill(new Integer[3], 17)), is(arrayContaining(1, null, 17)));
+		assertThat(fixed.toArray(Arrayz.fill(new Integer[7], 17)), is(arrayContaining(1, 2, 3, 4, 5, null, 17)));
+		assertThat(mutable.toArray(Arrayz.fill(new Integer[7], 17)), is(arrayContaining(1, 2, 3, 4, 5, null, 17)));
+		assertThat(sized.toArray(Arrayz.fill(new Integer[7], 17)), is(arrayContaining(1, 2, 3, 4, 5, null, 17)));
+		assertThat(unsized.toArray(Arrayz.fill(new Integer[7], 17)), is(arrayContaining(1, 2, 3, 4, 5, null, 17)));
 	}
 
 	@Test
 	public void add() {
 		expecting(UnsupportedOperationException.class, () -> empty.add(17));
 		expecting(UnsupportedOperationException.class, () -> single.add(17));
-		expecting(UnsupportedOperationException.class, () -> regular.add(17));
+		expecting(UnsupportedOperationException.class, () -> fixed.add(17));
 		expecting(UnsupportedOperationException.class, () -> mutable.add(17));
 		expecting(UnsupportedOperationException.class, () -> sized.add(17));
+		expecting(UnsupportedOperationException.class, () -> unsized.add(17));
 	}
 
 	@Test
@@ -202,12 +235,14 @@ public class IterableCollectionTest {
 		assertThat(empty.remove(17), is(false));
 
 		expecting(UnsupportedOperationException.class, () -> single.remove(1));
-		expecting(UnsupportedOperationException.class, () -> regular.remove(1));
+		expecting(UnsupportedOperationException.class, () -> fixed.remove(1));
 
 		assertThat(mutable.remove(1), is(true));
 		assertThat(mutable, contains(2, 3, 4, 5));
 
 		expecting(UnsupportedOperationException.class, () -> sized.remove(1));
+
+		expecting(UnsupportedOperationException.class, () -> unsized.remove(1));
 	}
 
 	@Test
@@ -217,23 +252,27 @@ public class IterableCollectionTest {
 		assertThat(single.containsAll(Lists.of(1)), is(true));
 		assertThat(single.containsAll(Lists.of(1, 2)), is(false));
 
-		assertThat(regular.containsAll(Lists.of(1, 2, 3, 4, 5)), is(true));
-		assertThat(regular.containsAll(Lists.of(1, 2, 3, 4, 5, 6)), is(false));
+		assertThat(fixed.containsAll(Lists.of(1, 2, 3, 4, 5)), is(true));
+		assertThat(fixed.containsAll(Lists.of(1, 2, 3, 4, 5, 6)), is(false));
 
 		assertThat(mutable.containsAll(Lists.of(1, 2, 3, 4, 5)), is(true));
 		assertThat(mutable.containsAll(Lists.of(1, 2, 3, 4, 5, 6)), is(false));
 
 		assertThat(sized.containsAll(Lists.of(1, 2, 3, 4, 5)), is(true));
 		assertThat(sized.containsAll(Lists.of(1, 2, 3, 4, 5, 6)), is(false));
+
+		assertThat(unsized.containsAll(Lists.of(1, 2, 3, 4, 5)), is(true));
+		assertThat(unsized.containsAll(Lists.of(1, 2, 3, 4, 5, 6)), is(false));
 	}
 
 	@Test
 	public void addAll() {
 		expecting(UnsupportedOperationException.class, () -> empty.addAll(Lists.of(17, 18, 19)));
 		expecting(UnsupportedOperationException.class, () -> single.addAll(Lists.of(17, 18, 19)));
-		expecting(UnsupportedOperationException.class, () -> regular.addAll(Lists.of(17, 18, 19)));
+		expecting(UnsupportedOperationException.class, () -> fixed.addAll(Lists.of(17, 18, 19)));
 		expecting(UnsupportedOperationException.class, () -> mutable.addAll(Lists.of(17, 18, 19)));
 		expecting(UnsupportedOperationException.class, () -> sized.addAll(Lists.of(17, 18, 19)));
+		expecting(UnsupportedOperationException.class, () -> unsized.addAll(Lists.of(17, 18, 19)));
 	}
 
 	@Test
@@ -241,7 +280,7 @@ public class IterableCollectionTest {
 		assertThat(empty.removeAll(Lists.of(1, 2)), is(false));
 
 		expecting(UnsupportedOperationException.class, () -> single.removeAll(Lists.of(1, 2)));
-		expecting(UnsupportedOperationException.class, () -> regular.removeAll(Lists.of(1, 2)));
+		expecting(UnsupportedOperationException.class, () -> fixed.removeAll(Lists.of(1, 2)));
 
 		assertThat(mutable.removeAll(Lists.of(17, 18)), is(false));
 		assertThat(mutable, contains(1, 2, 3, 4, 5));
@@ -250,6 +289,8 @@ public class IterableCollectionTest {
 		assertThat(mutable, contains(3, 4, 5));
 
 		expecting(UnsupportedOperationException.class, () -> sized.removeAll(Lists.of(1, 2)));
+
+		expecting(UnsupportedOperationException.class, () -> unsized.removeAll(Lists.of(1, 2)));
 	}
 
 	@Test
@@ -257,10 +298,10 @@ public class IterableCollectionTest {
 		assertThat(empty.retainAll(Lists.of(1, 2)), is(false));
 
 		assertThat(single.retainAll(Lists.of(1)), is(false));
-		assertThat(regular.retainAll(Lists.of(1, 2, 3, 4, 5)), is(false));
+		assertThat(fixed.retainAll(Lists.of(1, 2, 3, 4, 5)), is(false));
 
 		expecting(UnsupportedOperationException.class, () -> single.retainAll(Lists.of(2)));
-		expecting(UnsupportedOperationException.class, () -> regular.retainAll(Lists.of(6)));
+		expecting(UnsupportedOperationException.class, () -> fixed.retainAll(Lists.of(6)));
 
 		assertThat(mutable.retainAll(Lists.of(1, 2, 3, 4, 5)), is(false));
 		assertThat(mutable, contains(1, 2, 3, 4, 5));
@@ -269,6 +310,8 @@ public class IterableCollectionTest {
 		assertThat(mutable, contains(1, 2));
 
 		expecting(UnsupportedOperationException.class, () -> sized.retainAll(Lists.of(6)));
+
+		expecting(UnsupportedOperationException.class, () -> unsized.retainAll(Lists.of(6)));
 	}
 
 	@Test
@@ -295,11 +338,13 @@ public class IterableCollectionTest {
 		assertThat(empty, is(emptyIterable()));
 
 		expecting(UnsupportedOperationException.class, single::clear);
-		expecting(UnsupportedOperationException.class, regular::clear);
+		expecting(UnsupportedOperationException.class, fixed::clear);
 
 		mutable.clear();
 		assertThat(mutable, is(emptyIterable()));
 
 		expecting(UnsupportedOperationException.class, sized::clear);
+
+		expecting(UnsupportedOperationException.class, unsized::clear);
 	}
 }
